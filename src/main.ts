9819b530025c1f62cc8b7f2886119b43441ac399
asclaudino <arielsclaudino@gmail.com>
2024-08-15 11:50:52 -0300

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors({
     origin: 'https://file-uploader-tawny.vercel.app',
-    methods: Post,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
   await app.listen(3000);
