24b6b21351b304c8c6405afb37b176e4f98ccbac
asclaudino <arielsclaudino@gmail.com>
2024-08-15 11:45:54 -0300

    still fixing

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import dotenv from 'dotenv';
+import { Post } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors({
     origin: 'https://file-uploader-tawny.vercel.app',
+    methods: Post,
     credentials: true,
   }));
   await app.listen(3000);
